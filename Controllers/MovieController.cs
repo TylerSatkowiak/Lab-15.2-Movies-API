309a068b4a1ff41e6a24e5ab0589b4390ce002a0
Tyler Satkowiak <tylersatkowiak@quickenloans.com>
2020-09-23 09:28:31 -0400

    First commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Dapper.Contrib;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace Lab152MovieList.Controllers
+{
+    [Route("Movie")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        const string server = "Server=9QP7Q13\\SQLEXPRESS;Database=Slack;user id=sa;password=abc123";  //Tyler Database
+
+
+        [HttpGet]
+        public List<Movie> ReadAllMovies()
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> A = db.Query<Movie>($"select * from [Movies]").AsList<Movie>();
+            return A;
+
+        }
+
+        [HttpGet("Category/{Search}")]
+        public List<Movie> SearchCategory(string Search)
+        {
+
+            List<Movie> q;
+            q = Movie.SearchCategory(Search);
+            return q;
+        }
+
+        [HttpGet("MainActor/{Search}")]
+        public List<Movie> SearchMainActor(string Search)
+        {
+
+            List<Movie> q;
+            q = Movie.SearchMainActor(Search);
+            return q;
+        }
+
+        [HttpGet("Title/{Search}")]
+        public List<Movie> SearchTitle(string Search)
+        {
+
+            List<Movie> q;
+            q = Movie.SearchTitle(Search);
+            return q;
+        }
+
+        [HttpGet("Director/{Search}")]
+        public List<Movie> SearchDirector(string Search)
+        {
+
+            List<Movie> q;
+            q = Movie.SearchDirector(Search);
+            return q;
+        }
+
+        /*
+        [HttpGet("Search/{enter}")]
+        public List<Movie> Search(string Category, string enter)
+        {
+            
+            List<Movie> q;
+            if (Category.ToLower() == "director")
+            {
+                q = Movie.SearchDirector(enter);
+            }
+            else if (Category.ToLower() == "mainactor")
+            {
+                q = Movie.SearchMainActor(enter);
+            }
+            else
+            {
+                q = Movie.SearchTitle(enter);
+            }
+
+            return q;
+        }
+        */
+
+        [HttpGet("{ID}")]
+        public static Movie Read(int ID)
+        {
+            IDbConnection db = new SqlConnection(server);
+            Movie A = db.Get<Movie>(ID);
+            return A;
+        }
+
+
+        [HttpGet("Random")]
+        public static Movie RandomMovie()
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> A = db.Query<Movie>($"select * from [Movies]").AsList<Movie>();
+
+            Random rand = new Random();
+            int result = rand.Next(0, A.Count);
+            return A[result];
+        }
+
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Dapper.Contrib;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using Dapper.Contrib.Extensions;
+
+namespace Lab152MovieList
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string MainActor {get; set;} 
+        public string Director { get; set; }
+
+        
+        public string Category { get; set; }
+
+        const string server = "Server=9QP7Q13\\SQLEXPRESS;Database=Slack;user id=sa;password=abc123";  //Tyler Database
+
+
+        public static List<Movie> SearchTitle(string search)
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> Q = db.Query<Movie>($"select * from Movies where Title LIKE '%{search}%'").AsList();
+
+            return Q;
+        }
+
+        public static List<Movie> SearchMainActor(string search)
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> Q = db.Query<Movie>($"select * from Movies where MainActor LIKE '%{search}%'").AsList();
+
+            return Q;
+        }
+
+        public static List<Movie> SearchDirector(string search)
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> Q = db.Query<Movie>($"select * from Movies where Director LIKE '%{search}%'").AsList();
+
+            return Q;
+        }
+
+        public static List<Movie> SearchCategory(string search)
+        {
+            IDbConnection db = new SqlConnection(server);
+            List<Movie> Q = db.Query<Movie>($"select * from Movies where Category LIKE '%{search}%'").AsList();
+
+            return Q;
+        }
+    }
+}
